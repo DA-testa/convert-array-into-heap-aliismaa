b3411feac467577ae373b2a24a88261270aaeff5
Alisa Mamikova <116195263+aliismaa@users.noreply.github.com>
2023-03-19 16:07:04 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,35 +1,47 @@
 # python3
 
-
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    for i in range((n - 1) // 2, -1, -1):
+        sift_down(data, i, swaps)
     return swaps
 
+def sift_down(data, i, swaps):
+    n = len(data)
+    index = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left <= n - 1 and data[left] < data[index]:
+        index = left
+    if right <= n - 1 and data[right] < data[index]:
+        index = right
+
+    if i != index:
+        data[i], data[index] = data[index], data[i]
+        swaps.append((i, index))
+        sift_down(data, index, swaps)
 
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
+    choice = input("Choose the input method")
+    if "F" in choice:
+        filename = input()
+        if "a" in filename:
+            print("File names with letter a are not allowed")
+            return
+        with open("test/" + filename, 'r') as file:
+                    n = int(file.readline())
+                    data = list(map(int, file.readline().split()))
+                    assert len(data) == n
+                    swaps = build_heap(data)
+    elif "I" in choice:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+        swaps = build_heap(data)
 
 
     # output all swaps
