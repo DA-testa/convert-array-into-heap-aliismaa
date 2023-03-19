61bd45baf7fa9cfa2fbd941ea4732df347b31cd2
Alisa Mamikova <116195263+aliismaa@users.noreply.github.com>
2023-03-19 16:34:01 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,9 +2,10 @@
 
 def build_heap(data):
     swaps = []
+    # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
     n = len(data)
-    for i in range(n // 2, -1, -1):
+    for i in range((n - 1) // 2, -1, -1):
         sift_down(data, i, swaps)
     return swaps
 
@@ -14,9 +15,9 @@ def sift_down(data, i, swaps):
     left = 2 * i + 1
     right = 2 * i + 2
 
-    if left < n and data[left] < data[index]:
+    if left <= n - 1 and data[left] < data[index]:
         index = left
-    if right < n and data[right] < data[index]:
+    if right <= n - 1 and data[right] < data[index]:
         index = right
 
     if i != index:
@@ -24,32 +25,39 @@ def sift_down(data, i, swaps):
         swaps.append((i, index))
         sift_down(data, index, swaps)
 
+
 def main():
-   choice = input()
-    if "F" in choice:
-        filename = input()
-        if "a" in filename:
-            print("File names with letter a are not allowed")
-            return
-            try:
-                with open("test/" + filename, 'r') as file:
-                    n = int(file.readline())
-                    data = list(map(int, file.readline().split()))
-                    assert len(data) == n
-                    swaps = build_heap(data)
-            except FileNotFoundError:
-                print("Error")
-            return
-    elif "I" in choice:
+    
+    # TODO : add input and corresponding checks
+    # add another input for I or F 
+    # first two tests are from keyboard, third test is from a file
+    choice = input("Choose the input method")
+    if "I" in choice:
         n = int(input())
         data = list(map(int, input().split()))
         assert len(data) == n
         swaps = build_heap(data)
+    elif "F" in choice:
+        filename = input()
+        if "a" in filename:
+            print("File names with letter a are not allowed")
+            return
+
+        with open("test/" + filename, 'r') as file:
+                    n = int(file.readline())
+                    data = list(map(int, file.readline().split()))
+                    assert len(data) == n
+                    swaps = build_heap(data)
+    else:
+        print("Error")
+        exit()
+ 
 
     # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
 
 if __name__ == "__main__":
     main()
